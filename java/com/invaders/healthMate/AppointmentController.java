d09d22bbfc24c57b081768836151b8a2a98bf667
ThusithWB <96529753+ThusithWB@users.noreply.github.com>
2023-11-28 15:43:08 +0530

    Update AppointmentController.java
    Updated AppointmentController to send patient name, email, mobile number and e-mail to the appointments database.

--- java/com/invaders/healthMate/AppointmentController.java
+++ java/com/invaders/healthMate/AppointmentController.java
@@ -1,56 +1,74 @@
 package com.invaders.healthMate;
 
+import com.invaders.healthMate.dto.AppointmentDto;
+import com.invaders.healthMate.dto.RegistrationDto;
 import com.invaders.healthMate.model.Appointment;
 import com.invaders.healthMate.repository.AppointmentRepository;
 import com.invaders.healthMate.repository.DoctorRepository;
 import com.invaders.healthMate.repository.LocationRepository;
+import com.invaders.healthMate.AppointmentService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/appointments")
 public class AppointmentController {
 
     private final AppointmentRepository appointmentRepository;
+    private final AppointmentService appointmentService;
     private final LocationRepository locationRepository;
     private final DoctorRepository doctorRepository;
 
     @Autowired
     public AppointmentController(AppointmentRepository appointmentRepository,
+                                 AppointmentService appointmentService,
                                  LocationRepository locationRepository,
                                  DoctorRepository doctorRepository) {
         this.appointmentRepository = appointmentRepository;
+        this.appointmentService = appointmentService;
         this.locationRepository = locationRepository;
         this.doctorRepository = doctorRepository;
     }
 
     @GetMapping("/book")
     public String showBookingForm(Model model) {
-        model.addAttribute("appointment", new Appointment());
+        model.addAttribute("appointmentDto", new AppointmentDto());
         return "bookForm";
     }
 
     @PostMapping("/book")
-    public String bookAppointment(@ModelAttribute("appointment") @Valid Appointment appointment,
-                                  BindingResult result, Model model) {
-        if (result.hasErrors()) {
-            return "bookForm";
+    public ResponseEntity<Map<String, Object>> bookAppointment(@RequestBody Appointment appointment) {
+        try {
+            // Your logic for booking the appointment
+            appointmentService.bookAppointment(appointment);
+
+            // Construct the success response
+            Map<String, Object> response = new HashMap<>();
+            response.put("success", true);
+            response.put("message", "Appointment booked successfully!");
+
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            // Log the exception
+            e.printStackTrace();
+
+            // Construct the error response
+            Map<String, Object> response = new HashMap<>();
+            response.put("success", false);
+            response.put("error", "Failed to book the appointment. Please try again.");
+
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
         }
-
-        // Save the appointment to the database
-        appointmentRepository.save(appointment);
-
-        // You can redirect to the next step, such as selecting a location
-        return "redirect:/Location";
     }
 
-    // Add more methods for handling location selection, doctor selection, etc.
 }
-
