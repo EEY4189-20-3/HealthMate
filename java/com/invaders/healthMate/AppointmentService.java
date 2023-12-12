2b663f28255a6596c05800700b7e784f5c96b2c0
Thusith <96529753+ThusithWB@users.noreply.github.com>
2023-12-12 15:38:45 +0530

    included Java Mail Sender

--- java/com/invaders/healthMate/AppointmentService.java
+++ java/com/invaders/healthMate/AppointmentService.java
@@ -1,24 +1,30 @@
 package com.invaders.healthMate;
 
 import com.invaders.healthMate.dto.AppointmentDto;
-import com.invaders.healthMate.dto.RegistrationDto;
 import com.invaders.healthMate.model.Appointment;
-import com.invaders.healthMate.model.User;
 import com.invaders.healthMate.repository.AppointmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AppointmentService {
     private final AppointmentRepository appointmentRepository;
+    private final JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.from}") // Inject the 'from' email address from application.properties
+    private String fromEmail;
 
     @Autowired
-    public AppointmentService(AppointmentRepository appointmentRepository) {
+    public AppointmentService(AppointmentRepository appointmentRepository, JavaMailSender javaMailSender) {
         this.appointmentRepository = appointmentRepository;
+        this.javaMailSender = javaMailSender;
     }
-    public void processAppointment(AppointmentDto appointmentDto) {
-        validateAppointmentDto(appointmentDto);
 
+    public void bookAppointment(Appointment appointment) {
+        // Perform validation checks here if needed
         appointment.setPatientName(appointment.getPatientName());
         appointment.setPhoneNumber(appointment.getPhoneNumber());
         appointment.setAddress(appointment.getAddress());
@@ -28,19 +34,20 @@ public class AppointmentService {
         appointment.setAppointmentTime(appointment.getAppointmentTime());
 
         appointmentRepository.save(appointment);
+
+        // Send confirmation email
+        sendConfirmationEmail(appointment);
     }
 
-    private void validateAppointmentDto(AppointmentDto appointmentDto) {
+    private void sendConfirmationEmail(Appointment appointment) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(appointment.getEmail());
+        mailMessage.setSubject("Appointment Confirmation");
+        mailMessage.setText("Dear " + appointment.getPatientName() +
+                ",\n\nYour appointment on " + appointment.getAppointmentDate() +
+                " at " + appointment.getAppointmentTime() + " has been successfully booked.\n\nRegards,\nHealthMate Team");
+        mailMessage.setFrom(fromEmail);
 
-        // validation for non-empty fields
-        if (appointmentDto.getPatientName() == null || appointmentDto.getPatientName().isEmpty()) {
-            throw new IllegalArgumentException("Patient name cannot be empty");
-        }
-
-        if (appointmentDto.getEmail() == null || appointmentDto.getEmail().isEmpty()) {
-            throw new IllegalArgumentException("Email cannot be empty");
-        }
-
-        // Add more validation as needed...
+        javaMailSender.send(mailMessage);
     }
 }
